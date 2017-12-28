995e1ccb1d67446462edb8474cb0f5c2d678571b
Ronen Pinhasov <ronen@ronens-mbp.home>
2017-12-28 02:36:02 +0200

    add p json

--- app.js
+++ app.js
@@ -55,9 +55,11 @@ app.use(function (err, req, res, next) {
 });
 
 //database handler
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 // listen on port 3000
-app.listen(PORT, function () {
+app.listen(server_port, function () {
     console.log('Express app listening on port 8080 started at:'+ now());
 });
 var unirest = require('unirest');
